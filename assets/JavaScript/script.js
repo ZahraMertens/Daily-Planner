ff89870cf6b1401c50b29b5896a9fbdd90bce2c4
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-07-03 16:31:44 +1000

    JS comments

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,13 +1,23 @@
+//Daily-Planner JS 
 
+//Set timer in header and add interval that it is updated every second
 
 var timeDisplayEl = $("#time-display");
 
+$(document).ready(function() {
+    setInterval(function(){
+        var newTime = moment().format("MMM DD, YYYY [at] HH:mm:ss a");
+        timeDisplayEl.text(newTime);
+    }, 1000);
+});
 
-
+//Create moment.js starting every day from 8am
 var today = moment();
 today = today.startOf("Hour").hour(8);
 console.log(today);
 
+
+//PUSHING ALL TIMES FROM 8AM to 4PM IN THE FIRST COLUMN IN A NEW ROW
 //8am
 var time1 = today.add(0, "H");
 time1 = time1.format("HH:mm a");
@@ -15,12 +25,12 @@ console.log(time1)
 var timeContainer1 = $(".time-1").text(time1);
 
 //9am
-var time2 = today.add(1, "H");
+var time2 = today.add(1, "H"); //Increasing the time from 8am to 9am
 time2 = time2.format("HH:mm a");
 var timeContainer2 = $(".time-2").text(time2);
 
 //10am
-var time3 = today.add(1, "H");
+var time3 = today.add(1, "H"); //From 9am to 10am .......
 time3 = time3.format("HH:mm a");
 var timeContainer3 = $(".time-3").text(time3);
 
@@ -54,61 +64,65 @@ var time9 = today.add(1, "H");
 time9 = time9.format("HH:mm a");
 var timeContainer9 = $(".time-9").text(time9);
 
-//Set timer in header and add interval that it is updated every second
-$(document).ready(function() {
-    setInterval(function(){
-        var newTime = moment().format("MMM DD, YYYY [at] HH:mm:ss a");
-        timeDisplayEl.text(newTime);
-    }, 1000);
-});
 
-//Checking time to add classes for colours
+//Checking time to add classes to container for colours
 function checkTime () {
 
+    //Checking current time and round it to hour so the seconds and milli-seconds are 0...
     var currentTime = moment();
     currentTime = currentTime.startOf("hour");
     currentTime = currentTime.format("HH");
-
-    time1 = moment().startOf("hour").hour(8);
-    time1 = time1.format("HH");
 
     console.log(time1)
     console.log(currentTime)
 
    //8am
-    if (time1 === currentTime){
-        $("#input-text1").addClass("present"); //check before and after function
-    } else if (time1 > currentTime) {
+    time1 = moment().startOf("hour").hour(8);
+    time1 = time1.format("HH"); //Create new time1 var to round the time to hours (same as current time) to be able to compare current time and time1 var
+                                // using 24 h format that the time does not occur twice such as 12pm and 12am
+
+    if (time1 === currentTime){ //if the hour is the same (08 === 08) then add class present
+        $("#input-text1").addClass("present"); 
+
+    } else if (time1 > currentTime) { // if the current time hour is before calender time add class future
         $("#input-text1").addClass("future");
-    } else if (time1 <currentTime) {
+
+    } else if (time1 < currentTime) { //if the time1 is smaller than current time add class past for colours
         $("#input-text1").addClass("past");
     };
 
+    //9am
     time2 = moment().startOf("hour").hour(9);
     time2 = time2.format("HH");
-    //9am
+   
     if (time2 === currentTime){
         $("#input-text2").addClass("present");
+
     } else if (time2 > currentTime) {
         $("#input-text2").addClass("future");
+
     } else if (time2 < currentTime) {
         $("#input-text2").addClass("past");
     };
-
+    
+    //10am
     time3 = moment().startOf("hour").hour(10);
     time3 = time3.format("HH");
-    //10am
+    
     if (time3 === currentTime){
         $("#input-text3").addClass("present");
+
     } else if (time3 > currentTime) {
         $("#input-text3").addClass("future");
+
     } else if (time3 < currentTime) {
         $("#input-text3").addClass("past");
     };   
-
+    
+    //11am
     time4 = moment().startOf("hour").hour(11);
     time4 = time4.format("HH");
-    //11am
+    
     if (time4 === currentTime){
         $("#input-text4").addClass("present");
     } else if (time4 > currentTime) {
@@ -118,23 +132,29 @@ function checkTime () {
     };
 
     //12pm
-    time5 = moment().startOf("hour").hour(12);
+    time5 = moment().startOf("hour").hour(12); //12pm is hour 12 of the day
     time5 = time5.format("HH");
+
     if (time5 === currentTime){
         $("#input-text5").addClass("present");
+
     } else if (time5 > currentTime) {
         $("#input-text5").addClass("future"); 
+
     } else if (time5 < currentTime) {
         $("#input-text5").addClass("past");
     };
 
     //1pm
-    time6 = moment().startOf("hour").hour(13);
+    time6 = moment().startOf("hour").hour(13); //For example 1pm is hour 13 of the day
     time6 = time6.format("HH");
+
     if (time6 === currentTime){
         $("#input-text6").addClass("present");
+
     } else if (time6 > currentTime) {
         $("#input-text6").addClass("future");
+
     } else if (time6 < currentTime) {
         $("#input-text6").addClass("past");
     };
@@ -142,10 +162,13 @@ function checkTime () {
     //2pm
     time7 = moment().startOf("hour").hour(14);
     time7 = time7.format("HH");
+
     if (time7 === currentTime){
         $("#input-text7").addClass("present");
+
     } else if (time7 > currentTime) {
         $("#input-text7").addClass("future");
+
     } else if (time7 < currentTime) {
         $("#input-text7").addClass("past");
     };
@@ -153,10 +176,13 @@ function checkTime () {
     //3pm
     time8 = moment().startOf("hour").hour(15);
     time8 = time8.format("HH");
+
     if (time8 === currentTime){
         $("#input-text8").addClass("present");
+
     } else if (time8 > currentTime) {
         $("#input-text8").addClass("future");
+
     } else if (time8 < currentTime) {
         $("#input-text8").addClass("past");
     };
@@ -164,38 +190,47 @@ function checkTime () {
     //4pm
     time9 = moment().startOf("hour").hour(16);
     time9 = time9.format("HH");
+
     if (time9 === currentTime){
         $("#input-text9").addClass("present");
+
     } else if (time9 > currentTime) {
         $("#input-text9").addClass("future");
+
     } else if (time9 < currentTime) {
         $("#input-text9").addClass("past");
     };
 }
-
+//Execute function to check time
 checkTime();
 
+//Function to add input to local storage
 $(".saveBtn").on("click", function() {
     
-    var input = $(this).siblings("textarea").val();
-    var hour = $(this).siblings().attr("id");
+   
+    var input = $(this).siblings("textarea").val(); //Getting input from any textarea element
+    var hour = $(this).siblings().attr("id"); //Selecting the parent container with specific id
 
-    localStorage.setItem(hour, input);
+    console.log(hour);
+    console.log(input);
+
+    localStorage.setItem(hour, input); //for each text area setting new localStorage key and value while the key is the specific id element
 });
 
-
+//On Double-Click the user is able to remove text inputs from local storage
 $(".saveBtn").dblclick(function() {
-
 
     var input = $(this).siblings("textarea").val();
     var hour = $(this).siblings().attr("id");
 
+    //If there is an input on double click the user removes the specific id element and textareas value
     if (input !== null){
     localStorage.removeItem(hour, input);
     window.location.reload();
     }
 });
 
+//Render input to specific textarea depending on element with specific id
   $("#input-text1").val(localStorage.getItem("hour8"));
   $("#input-text2").val(localStorage.getItem("hour9"));
   $("#input-text3").val(localStorage.getItem("hour10"));
